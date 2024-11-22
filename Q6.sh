46acd31992b5ca37705b583a261f337ca8bcf22a
rafeedav <rafeedav321@gmail.com>
2024-11-22 16:38:08 +0530

    file_wordcount

--- Q6.sh
+++ Q6.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-read -p " Enter a filename :" a
-file_word=$(cat "$a" | wc -w )
-echo "number of words in the file is :$file_word "
+read -p "Enter a filename :" a
+read -p "Enter a specific word :" b
+file_word=$(grep -o -i "$b" "$a" | wc -l)
+echo "Number of word "$b" in "$a" is : $file_word "
